253509dfa0027eb3835458f198109ab42b173036
Shola Ayeni <ayenisholah@yahoo.com>
2019-11-05 15:56:04 +0100

    created Queue data structure

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,6 +1,18 @@
-from util import Stack, Queue  # These may come in handy
+class Queue():
+    def __init__(self):
+        self.queue = []
 
-q = Queue()
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
 
 
 class Graph:
@@ -32,4 +44,9 @@ class Graph:
 
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+
+    # add parents to graph
+    for parents in ancestors:
+        graph.add_vertex(parents[0])
+        graph.add_vertex(parents[1])
