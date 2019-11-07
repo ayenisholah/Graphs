71ffcfbea0a25ad56ce781c5687ed281c489ac53
Shola Ayeni <ayenisholah@yahoo.com>
2019-11-07 16:55:17 +0100

    earliest ancestor solution

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,57 +1,80 @@
-class Queue():
+class Queue:
     def __init__(self):
-        self.queue = []
+        self.storage = []
 
     def enqueue(self, value):
-        self.queue.append(value)
+        self.storage.append(value)
 
     def dequeue(self):
         if self.size() > 0:
-            return self.queue.pop(0)
+            return self.storage.pop(0)
         else:
             return None
 
     def size(self):
-        return len(self.queue)
+        return len(self.storage)
 
 
 class Graph:
-    """
-        Represent a graph as a dictionary of vertices mapping labels to edges.
+    """ 
+        Represent a graph as a dictionary of verts 
+        mapping labels to edges 
+
     """
 
     def __init__(self):
         self.vertices = {}
 
-    def add_vertex(self, vertex):
-        """
-            Add a vertex to the graph
-        """
-        self.vertices[vertex] = set()
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
-        """
-            Add a directed edge to the graph
-        """
-        # Check if v1 and v2 exists in vertices list
         if v1 in self.vertices and v2 in self.vertices:
-            # add v2 at v1 of vertices
             self.vertices[v1].add(v2)
-        # Otherwise
         else:
-            # Raise and error
-            raise KeyError("That vertex does not exist")
+            raise KeyError("That vertex does not exist!")
 
 
 def earliest_ancestor(ancestors, starting_node):
+    # 1. Build the graph
+    # instantiate a new graph object
     graph = Graph()
-
-    # load pairs to graph
-    for parents in ancestors:
-        graph.add_vertex(parents[0])
-        graph.add_vertex(parents[1])
-
-    #   {1 : {3}, 2: {3}, 3: {6}, 4: {5, 8}, 5: {6, 7}, 6: {}, 7:{} 8: {9}, 9:{}, 10: {1}, 11: {8}}
-
-    # edges
-
+    # loop over all pairs in ancestors
+    for pair in ancestors:
+        # add pair[0] and pair[1] to the graph
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+        # build the edges in reverse
+        graph.add_edge(pair[1], pair[0])
+    # Do a BFS (with paths)
+    # create a queue
+    q = Queue()
+    # enqueue starting node inside a list
+    q.enqueue([starting_node])
+    # set a max path length to 1
+    max_path_length = 1
+    # set initial earlyest ancestor
+    earliest_ancestor = -1
+    # while queue has contents
+    while q.size() > 0:
+        # dequeue the path
+        path = q.dequeue()
+        # get the last vert
+        vert = path[-1]
+        # if path is longer or equal and the value is smaller, or if the path is longer
+        if (len(path) >= max_path_length and vert < earliest_ancestor) or (len(path) > max_path_length):
+            # set the earliest ancestor to the vert
+            earliest_ancestor = vert
+            # set the max path length to the len of the path
+            max_path_length = len(path)
+        # loop over each neighbor in the graphs vertices at index of vert
+        for neighbor in graph.vertices[vert]:
+            # make a copy of the path
+            path_copy = list(path)
+            # append neighbor to the coppied path
+            path_copy.append(neighbor)
+            # then enqueue the copied path
+            q.enqueue(path_copy)
+    # return earliest ancestor
+    return earliest_ancestor
